1715fb556b66fc649e3e4fa31a673cf91064c381
zeroxys <miguelzavalac@gmail.com>
2018-02-15 19:50:50 -0600

    adding splashscreen and change the android icon

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import SplashScreen from 'react-native-splash-screen'
 
 export default class App extends React.Component {
+
+  componentDidMount() {
+    SplashScreen.hide()
+  }
+  
   render() {
     return (
       <View style={styles.container}>
